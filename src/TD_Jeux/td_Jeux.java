00e05abacdaefee5d04832a430f4ef62e0217130
Evan-POTIN <evan.potin@etu.umontpellier.fr>
2020-09-29 16:22:31 +0200

    first commit

--- src/TD_Jeux/td_Jeux.java
+++ src/TD_Jeux/td_Jeux.java
@@ -1,14 +1,13 @@
 package TD_Jeux;
 
 public class td_Jeux {
-    public static void main(String[] var0) {
-        resoudreAux(5,1,2);
+    public void main(String[] var0) {
+        resoudre(5);
     }
 
-    public static void tourHanoi() {
-
+    private static void resoudre(int n){
+        resoudreAux(n,1,3);
     }
-
     public static void resoudreAux(int n, int i, int j) {
         int k = 6 - i - j;
         if (n == 1) {
@@ -17,6 +16,13 @@ public class td_Jeux {
             resoudreAux(n-1,i,k);
             System.out.println(i + " -> " + n);
             resoudreAux(n-1,k,j);
+        }
+    }
+    public int nbCoup(int n){
+        if (n==1){
+            return 1;
+        }else if(n>= 2){
+            return 2*nbCoup(n-1) + 1;
         }
     }
 }
